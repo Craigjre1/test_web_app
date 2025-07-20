d1be44c40f2a27119ac4765e69bab52556c7c880
Craigjre1 <craigjre1@googlemail.com>
2025-07-20 22:59:30 +0100

    updated to include deletion and marking for completion routines. Need to update HTML

--- app.py
+++ app.py
@@ -1,31 +1,48 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 tasks = [
-    {"id": 1, "description": "Buy groceries"},
-    {"id": 2, "description": "Write blog post"},
-    {"id": 3, "description": "Build website"}
+    {"id": 1, "description": "Buy groceries","completed":False},
+    {"id": 2, "description": "Write blog post","completed":False},
+    {"id": 3, "description": "Build website","completed":False}
 ]
 
+nextID = 4
+
 @app.route("/",methods=['POST','GET'])
-def hello_world():
+def home():
+    global tasks
     if request.method == 'POST':
         new_task = request.form.get("task")
         if new_task and new_task.strip():
-            add_dict_tolist(new_task,tasks) # will also renumber tasks, allowing for deletions
+            add_dict_tolist(new_task)
 
     return render_template("home.html",task_list=tasks)
 
-def add_dict_tolist(item,my_list):
-    new_ID = 1
-    for my_dict in my_list: #renumber all existing items in the list (allowing for deletions)
-        my_dict["id"] = new_ID
-        new_ID+=1
-    
-    new_dict = {"id": new_ID,"description":item} #create dictionary for new item
-    my_list.append(new_dict) # append new dictionary to the list with new numbering
+@app.route("/delete/<int:id_num>", methods=['POST','GET'])
+def delete(id_num):
+    global tasks
+    if request.method =='POST':
+        tasks = [task for task in tasks if task['id']!=id_num]
 
+    return redirect(url_for('home'))
+
+@app.route("/complete/<int:id_num>", methods=['POST','GET'])
+def complete(id_num):
+    global tasks
+    if request.method =='POST':
+        for task in tasks:
+            if task['id']==id_num:
+                task['completed']=True
+
+    return redirect(url_for('home'))
+
+def add_dict_tolist(item) -> None:
+    global nextID
+    new_dict = {"id": nextID,"description":item,"completed":False} #create dictionary for new item
+    tasks.append(new_dict) # append new dictionary to the list with new numbering
+    nextID +=1
 
 if __name__=='__main__':
     app.run(debug=True)
