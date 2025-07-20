66fe64562fd05cadcc919a4194e36de6ac8a709c
Craigjre1 <craigjre1@googlemail.com>
2025-07-20 15:59:56 +0100

    Added home .HTML with inputs and POST+GET methods in app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello world")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+tasks = ["Buy groceries", "Write blog post", "Call Mum"]
+
+@app.route("/")
+def hello_world():
+    return render_template("home.html",task_list=tasks)
+
+if __name__=='__main__':
+    app.run(debug=True)
