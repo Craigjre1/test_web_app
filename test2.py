2b956efd5cc231a6b27180a152cee32c87b255cb
Craigjre1 <craigjre1@googlemail.com>
2025-07-24 22:26:23 +0100

    updated prompts on ReAct tool - not very successful so far!

--- test2.py
+++ test2.py
@@ -1,19 +1,23 @@
 import ollama
 
-SYSTEM_PROMPT = """You are a smart AI agent that can use tools to answer questions.
+SYSTEM_PROMPT = """You are a smart AI agent that can use tools to answer questions. Your observations can ONLY
+come from the output of using a tool - do NOT guess. If no observations come back, continue with your reasoning
+you'll have the opportunity to iterate and re-use a tool and try again with different queries or parameters.
 
 You must think step by step. Use the following format:
 
 Thought: <your reasoning>
 Action: <tool name>
 Action Input: <input for the tool>
-Observation: <result of tool>
+Observation: <result of a tool>
 ... (repeat Thought/Action/Observation as needed).
 Final Answer: <your final answer>
 
 Available tools:
 - search_tool(query: str): Searches the web for weather information.
 - convert_to_F(temp: float): Converts celsius to farenheit.
+
+If no response from a tool, say so - do not guess a result.
 """
 
 search_return_dict= {1:"Unable to return suitable search results for your query",
@@ -29,7 +33,22 @@ def convert_to_F(temp):
     return farenheit
 
 def react_agent(user_query,max_steps):
-    return 1
+    history = [SYSTEM_PROMPT,f'User query: {user_query}']
+
+    for step in range(max_steps):
+        prompt = "\n".join(history)
+
+    response = ollama.chat(
+        model="phi3:mini",
+        messages=[{"role": "user", "content": prompt}],
+        options={'temperature': 0}
+)          
+    return response['message']['content']
+
+tool_map = {"search_tool":search_tool,
+            "convert_to_F":convert_to_F
+}
+
 
 user_input = input("What weather question would you like to ask? ")
-print(react_agent(user_input,5))
+print(react_agent(user_input,1))
